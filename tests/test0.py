18b3e72c498d8fe1fdef3fd68764d7786d4d72c8
James Dalessio <jrd261@gmail.com>
2015-09-18 16:24:04 -0400

    Add socket io dependency

--- tests/test0.py
+++ tests/test0.py
@@ -6,6 +6,9 @@ class TestThatTestingWorks(unittest.TestCase):
   def test_something(self):
     self.assertEqual(0, 0)
 
+  def test_something_else(self):
+    self.assertEqual(0, 0)
+
 
 if __name__ == '__main__':
   unittest.main()
