fc13e0ab6d36c1d0bdf0536e13dd441e30078f4f
James Dalessio <jrd261@gmail.com>
2015-09-18 16:34:49 -0400

    Simplify tests into boilerplate.

--- tests/test0.py
+++ tests/test0.py
@@ -1,14 +1,10 @@
 import unittest
 import scala
 
-class TestThatTestingWorks(unittest.TestCase):
+class TestBoilerplate(unittest.TestCase):
 
   def test_something(self):
     self.assertEqual(0, 0)
-
-  def test_something_else(self):
-    self.assertEqual(0, 0)
-
 
 if __name__ == '__main__':
   unittest.main()
