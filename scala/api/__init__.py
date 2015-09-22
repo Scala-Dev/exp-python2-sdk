09b94a87fbc88c45746af1b7657d326e88efc30a
James Dalessio <jrd261@gmail.com>
2015-09-22 15:21:26 -0400

    Namespaces to verbs in api.

--- scala/api/__init__.py
+++ scala/api/__init__.py
@@ -1,4 +1,60 @@
-from . import devices
-from . import experiences
-from . import zones
-from . import locations
+from scala.lib import api_utils
+from scala.lib.models.device import Device
+from scala.lib.models.location import Location
+from scala.lib.models.experience import Experience
+
+
+""" Devices """
+
+def getDevices(**params):
+  query = api_utils.get('/api/devices', params=params)
+  empty = []
+  return [Device(x, _new=False) for x in query.get("results", empty)]
+
+def getDevice(uuid):
+  return Device(api_utils.get('/api/devices' + uuid), _new=False)
+
+def createDevice(document):
+  return Device(document).save()
+
+
+""" Experiences """
+
+def getExperiences(**params):
+  query = api_utils.get('/api/experiences', params=params)
+  empty = []
+  return [Experience(x, _new=False) for x in query.get("results", empty)]
+
+def getExperience(uuid):
+  return Experience(api_utils.get('/api/experiences' + uuid), _new=False)
+
+def createExperience(document):
+  return Experience(document).save()
+
+
+""" Locations """
+
+def getLocations(**params):
+  query = api_utils.get('/api/locations', params=params)
+  empty = []
+  return [Location(x, _new=False) for x in query.get("results", empty)]
+
+def getLocation(uuid):
+  return Location(api_utils.get('/api/locations' + uuid), _new=False)
+
+def createLocation(document):
+  return Location(document).save()
+
+
+""" Zones """
+
+def getZones(**params):
+  query = api_utils.get('/api/zones', params=params)
+  empty = []
+  return [Zone(x, _new=False) for x in query.get("results", empty)]
+
+def getZone(uuid):
+  return Zone(api_utils.get('/api/zones' + uuid), _new=False)
+
+def createZone(document):
+  return Zone(document).save()
