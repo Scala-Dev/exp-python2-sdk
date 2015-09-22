39b6b19fb513a656bdfdbe2c5970453b6555149c
James Dalessio <jrd261@gmail.com>
2015-09-22 17:11:20 -0400

    whoops, get rid of camel case

--- scala/api/__init__.py
+++ scala/api/__init__.py
@@ -6,55 +6,55 @@ from scala.lib.models.experience import Experience
 
 """ Devices """
 
-def getDevices(**params):
+def get_devices(**params):
   query = api_utils.get('/api/devices', params=params)
   empty = []
   return [Device(x, _new=False) for x in query.get("results", empty)]
 
-def getDevice(uuid):
+def get_device(uuid):
   return Device(api_utils.get('/api/devices' + uuid), _new=False)
 
-def createDevice(document):
+def create_device(document):
   return Device(document).save()
 
 
 """ Experiences """
 
-def getExperiences(**params):
+def get_experiences(**params):
   query = api_utils.get('/api/experiences', params=params)
   empty = []
   return [Experience(x, _new=False) for x in query.get("results", empty)]
 
-def getExperience(uuid):
+def get_experience(uuid):
   return Experience(api_utils.get('/api/experiences' + uuid), _new=False)
 
-def createExperience(document):
+def create_experience(document):
   return Experience(document).save()
 
 
 """ Locations """
 
-def getLocations(**params):
+def get_locations(**params):
   query = api_utils.get('/api/locations', params=params)
   empty = []
   return [Location(x, _new=False) for x in query.get("results", empty)]
 
-def getLocation(uuid):
+def get_location(uuid):
   return Location(api_utils.get('/api/locations' + uuid), _new=False)
 
-def createLocation(document):
+def create_location(document):
   return Location(document).save()
 
 
 """ Zones """
 
-def getZones(**params):
+def get_zones(**params):
   query = api_utils.get('/api/zones', params=params)
   empty = []
   return [Zone(x, _new=False) for x in query.get("results", empty)]
 
-def getZone(uuid):
+def get_zone(uuid):
   return Zone(api_utils.get('/api/zones' + uuid), _new=False)
 
-def createZone(document):
+def create_zone(document):
   return Zone(document).save()
