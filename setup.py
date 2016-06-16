4000c64d3b553a62eac0b7a2ed540341be105b11
James Dalessio <jrd261@gmail.com>
2016-06-16 12:31:22 -0400

    Change version to 1.0.3

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-
 from setuptools import setup
 
 setup(
   name='exp-sdk',
   packages= ['exp_sdk'],
-  version='1.0.3b1',
+  version='1.0.3',
   description='EXP Python SDK',
   author='Scala',
   author_email='james.dalessio@scala.com',
   url='https://github.com/scalainc/exp-python2-sdk',
-  download_url='https://github.com/scalainc/exp-python2-sdk/tarball/1.0.3b1',
+  download_url='https://github.com/scalainc/exp-python2-sdk/tarball/1.0.3',
   install_requires=["requests", "socketIO_client"],
   license='MIT',
   keywords=['scala', 'exp', 'sdk', 'signage'],
